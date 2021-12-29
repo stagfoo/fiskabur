8ba54d053d4b868300694e0fec3b5a097f352790
Alex King <alex@basal.dev>
2021-12-29 19:18:45 +0900

    🍱　refactor ░ remove unused deps

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-import time
-import sys
-import random
 import webview
 
-
 class Api:
-
     def getImage(self):
         file_types = ('Image Files (*.bmp;*.jpg;*.gif)', 'All files (*.*)')
         result = window.create_file_dialog(webview.OPEN_DIALOG, allow_multiple=True, file_types=file_types)
@@ -14,8 +8,6 @@ class Api:
             'message': result
         }
         return response
-
-
 
 if __name__ == '__main__':
     api = Api()
